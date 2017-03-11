5ac8015523283a6629dd45f6b2ff8f86e98462b5
mlaina <laina855@gmail.com>
2017-03-11 14:29:46 +0100

    all project

---
+++ Assets/EnergyBar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyBar : MonoBehaviour {
+
+	private RectTransform rt;
+	private float totalEnergy;
+
+	public float value = 100f;
+
+	// Use this for initialization
+	void Start () {
+		this.rt = GetComponent<RectTransform> ();
+		this.totalEnergy = rt.sizeDelta.x;
+	}
+
+	void Update() {
+		float valor = 100.0f - this.value;
+		setPercentageOfEnergy (valor);
+	}
+
+	//newValue es el porcentaje restante
+	private void setPercentageOfEnergy(float newValue) {
+		float x = (newValue * totalEnergy) / 100;
+		float y = rt.localPosition.y;
+		float z = rt.localPosition.z;
+
+		rt.localPosition = new Vector3 (-x, y, z);
+	}
+
+
+}
---
+++ Assets/MainWindowManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainWindowManager : MonoBehaviour {
+
+	public void WindowClick()
+    {
+        SceneManager.LoadScene("Level_Selector");
+    }
+}
